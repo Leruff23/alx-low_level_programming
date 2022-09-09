7971189b8a2ff17f2172a8359a7f4f494e98d4fd
Your name <you@example.com>
2022-09-08 18:54:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - entry point
  *
  * Return: 0 after printing the fonction
  */
 int main(void)
 { puts("\"Programming is like building a multilingual puzzle");
-  return (0); }
+  return (0);
+}
+
 
