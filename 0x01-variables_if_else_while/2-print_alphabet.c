f2732e218503bfde5e520adb168f611ffd9f8c35
Your name <you@example.com>
2022-09-13 11:10:27 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,11 @@
 int main(void)
 
 {
-  char alphabet;
+char alphabet;
 
-  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-    {
-      putchar(alphabet);
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
 }
 
 putchar('\n');
