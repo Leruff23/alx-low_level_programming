d78b0f83ed79b0c93fd39c9a330cd0ddc7df039d
Your name <you@example.com>
2022-09-14 17:57:21 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * jack bauer - func that print evry minute of the day Jack Bauer, starti
- * n from 00:00 to 23:59, min loop counts min, while hour loop count hours
+ * n from 00:00 to 23:59, min loop counts mins, while hour loop count hours
  * and resets mins
  * Return: 0
  */
