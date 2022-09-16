4807862873e9b9a918a260d430197ee3fdc1ca1f
Your name <you@example.com>
2022-09-16 16:52:06 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isupper - check if a character is upper case or not
+ * _isupper - checks if a character is upper case or not
  * @c: character to be tested
  * Return: 1 whether it is, 0 otherwise
  */
 
 int _isupper(int c)
 {
-if((c >= 'A') && (c >= 'Z'))
+if((c >= 'A') && (c <= 'Z'))
 {
 return(1);
 }
