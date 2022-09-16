d26698fbc7f96884ced030500f266e09fdceaf1b
Your name <you@example.com>
2022-09-16 14:56:12 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -11,5 +12,5 @@ int i;
 i = 98;
 positive_or_negative(i);
 
- return (0);
+return (0);
 }
