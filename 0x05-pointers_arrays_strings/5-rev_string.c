d55be99c1afeba9d04f82a060366155479544fd4
Your name <you@example.com>
2022-09-19 17:13:13 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,15 @@
  *Return: void
  */
 
-void rev_strinng(char *s)
+void rev_string(char *s)
 {
-char *sart_c, *end_c, c;
+char *start_c, *end_c, c;
 int i, count;
-int lenght = 0;
+int length = 0;
 
-for (i = 0, s[i], i++)
+for (i = 0; s[i], i++)
 {
-lenght++;
+length++;
 }
 
 count = length;
