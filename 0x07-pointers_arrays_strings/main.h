a132bd59ae18e25af8e1fb083bb4968f202c67c8
Your name <you@example.com>
2022-09-27 11:12:04 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,4 @@
-#ifndef MAIN_H
 #define MAIN_H
-
-/**
- * void prototypes()
- * int prototypes()
- */
 
 int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
@@ -16,5 +10,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif /*MAIN_H*/
