7b41d91bd7af34987927ee53bbc67a88dbdf05b4
Your name <you@example.com>
2022-10-04 07:17:01 -0700

    task0

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy a memory area from src to dest
+ * @dest: memory area to be copied to
+ * @src: memory area to be copied from
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to the copied memory block
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+
+return (dest);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolue value of an integrer
+ * @c: is the int that will use for the argument of the function
+ * Return: 0
+ */
+int _abs(int c)
+{
+if (c > 0 || c == 0)
+{
+return (c);
+}
+else
+return (c * -1);
+}
