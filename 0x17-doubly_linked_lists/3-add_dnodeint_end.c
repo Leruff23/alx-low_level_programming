1d223110fbeecfacd3ed79811f87a19cd9ed731a
Your name <you@example.com>
2022-12-12 13:16:48 -0800

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -4,7 +4,7 @@
  * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
  * @head: dlistint_t list to add the node to
  * @n: number included in the new node
- * Return: the new node or NULL if failed
+ *Return: the new node or NULL if failed
  */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
